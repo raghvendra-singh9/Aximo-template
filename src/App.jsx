75258c32762d3af917f70f637d5a56790896030a
Raghvendra Singh Shekhawat <163868970+raghvendra-singh9@users.noreply.github.com>
2024-08-22 22:20:05 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import "./assets/styles/main.scss";
+import Header from "./components/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
